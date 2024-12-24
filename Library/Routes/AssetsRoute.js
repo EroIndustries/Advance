f9e164cd9fa2fd796ad9cc5cc2947b325b05d122
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2024-12-24 19:54:23 +0300

    Auto Update
    We have Updated The Configurations With More Better Features and More to Come

--- Library/Routes/AssetsRoute.js
+++ Library/Routes/AssetsRoute.js
@@ -1,13 +1,9 @@
 import { APPICON } from "../Assets/Icons/White/App/AndroidApp.js";
 import { BACKICON } from "../Assets/Icons/White/Back/BackIcon.js";
-
 const ASSETSROUTE=()=>{
-
 const FUNCTIONS=`const WHITEAPPICON='${APPICON}';
 const WHITEBACKICON='${BACKICON}';
 `;
 localStorage.setItem("ICONS",FUNCTIONS);
-
 };
-
 export{ASSETSROUTE}
--- Library/Routes/GeneralRoute.js
+++ Library/Routes/GeneralRoute.js
@@ -3,7 +3,6 @@ import { MODULE } from "../Module/Module.js";
 import { ASSETSROUTE } from "./AssetsRoute.js";
 import { COMPONENTSROUTE } from "./ComponentsRouter.js";
 import { FUNCTIONSROUTE } from "./FunctionsRoute.js"
-
 const GENERALROUTE=()=>{
 
     APIS();
@@ -17,5 +16,4 @@ const GENERALROUTE=()=>{
     ASSETSROUTE();
 
 };
-
 export{GENERALROUTE};
