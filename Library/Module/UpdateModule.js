9edc8f4407545013aaab4eeda2a08eb91c625928
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2024-12-24 13:52:50 +0300

    Environment Updater
    We have Added the Updater Function to Check if the User is Development Mode or Production

--- Library/Module/UpdateModule.js
+++ Library/Module/UpdateModule.js
@@ -1,20 +1,42 @@
 const APPUPDATER=()=>{
 
-    import('../Start/Start.js')
-    .then(module =>
-        {
-            if (typeof module.APPLOAD === 'function') {
-                module.APPLOAD();
-            } else {
-                console.error('APPLOAD is not defined in the module');
+    if (localStorage.getItem("Environment" === 'Development' )) {
+
+        import('../Start/Start.js')
+        .then(module =>
+            {
+                if (typeof module.APPLOAD === 'function') {
+                    module.APPLOAD();
+                } else {
+                    console.error('APPLOAD is not defined in the module');
+                }
             }
-        }
-    )
-    .catch(error =>
-        {
-            console.error('Error loading the module:', error);
-        }
-    ); 
+        )
+        .catch(error =>
+            {
+                console.error('Error loading the module:', error);
+            }
+        ); 
+        
+    } else {
+
+        import('https://eroindustries.github.io/Advance/Start/Start.js')
+        .then(module =>
+            {
+                if (typeof module.APPLOAD === 'function') {
+                    module.APPLOAD();
+                } else {
+                    console.error('APPLOAD is not defined in the module');
+                }
+            }
+        )
+        .catch(error =>
+            {
+                console.error('Error loading the module:', error);
+            }
+        ); 
+        
+    }
 
 }
 
