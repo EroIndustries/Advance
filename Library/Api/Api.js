bfc5927dbf5d90a7f130b367c94bccd41b3e78da
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2024-12-24 19:27:26 +0300

    Update Api.js
    We have Introduced New Apis That are Going to Ease The DataDase Connection Of The Company Apps

--- Library/Api/Api.js
+++ Library/Api/Api.js
@@ -1,5 +1,10 @@
 const LINKS=`
+const UPDATEDATABASECONNECTION='https://script.google.com/macros/s/AKfycbzbmRB2zfDyMcde9mSmgC8u6U6a805g-LWGj9zRf7CZwrGp8eWwOPZanZ4htoNjEStGlQ/exec';
+const GETDATABASECONNECTION='https://script.google.com/macros/s/AKfycby5S_I3_hu6iEB0n8I7cmFZ9liA1kGPpQ-9ewmTcJmkUuMCYY540oaGDwJGE4ADs6tv5w/exec';
+const INSERTDATABASECONNECTION='https://script.google.com/macros/s/AKfycbyfMnGzbYAbyK5TzUZ2Og9IH8QDlG81yUydnWQ5fCLzJYHj9Y36KvpaeYZoLYjQ5iDsUQ/exec';
+const CREATEDATABASECONNECTION='https://script.google.com/macros/s/AKfycbxHQH_kk7ovfIaHdXtsD_z2dqXQE1nwSO3W6mpYBPXhXfFpF2JOK72wV_ebsYjgpEHY9g/exec';
 const CREATETABLECONNECTION='https://script.google.com/macros/s/AKfycbzhovbTYHMwWgfZUq9A3Z3akO6HEpE2OB4LKjSkWF5fx1-WUKc4dJ2MhVcgIyajEaX6qw/exec';
+const COMPOSEEMAILCONNECTION='https://script.google.com/macros/s/AKfycbzadYn-_BI8doMkz1okkprjTw7wFw9S09rbhjjJhsUDBveO95r0NnX5dljHc4hfLJgDjQ/exec';
 `;
 const APIS=()=>{
     localStorage.setItem('APIS',LINKS);
