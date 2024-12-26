d976f7e40511088110ebf4c477bbd6bcf29ca742
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2024-12-26 10:39:13 +0300

    Update UpdateModule.js
    We are now The cloud Connection For The Frame Work ,Even Those in Remotes Areas have Access to the FrameWork

--- Library/Module/UpdateModule.js
+++ Library/Module/UpdateModule.js
@@ -1,21 +1,40 @@
 const APPUPDATER=()=>{
     if (navigator.onLine) {
-        import('../Start/Start.js')
-        .then(module =>
-            {
-                if (typeof module.APPLOAD === 'function') {
-                    module.APPLOAD();
-                } else {
-                    console.error('APPLOAD is not defined in the module');
-                };
-            }
-        )
-        .catch(error =>
-            {
-                console.error('Error loading the module:', error);
-            }
-        ); 
-        return;
+        if (localStorage.getItem('Environment') === 'Development' ) {
+
+            import('../Start/Start.js')
+            .then(module =>
+                {
+                    if (typeof module.APPLOAD === 'function') {
+                        module.APPLOAD();
+                    } else {
+                        console.error('APPLOAD is not defined in the module');
+                    };
+                }
+            )
+            .catch(error =>
+                {
+                    console.error('Error loading the module:', error);
+                }
+            );  
+        } else {
+
+            import('../Start/Start.js')
+            .then(module =>
+                {
+                    if (typeof module.APPLOAD === 'function') {
+                        module.APPLOAD();
+                    } else {
+                        console.error('APPLOAD is not defined in the module');
+                    };
+                }
+            )
+            .catch(error =>
+                {
+                    console.error('Error loading the module:', error);
+                }
+            ); 
+        };
     };
 };
 export{APPUPDATER};
