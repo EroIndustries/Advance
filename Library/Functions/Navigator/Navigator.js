fc7789cc900d727abd44c706df555b916b3cda4b
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2024-12-27 15:25:36 +0300

    Minor Corrections
    Updated Minor Features

--- Library/Functions/Navigator/Navigator.js
+++ Library/Functions/Navigator/Navigator.js
@@ -5,20 +5,20 @@ const NAVIGATOR = (NEWPAGE, FUNCTION, FUNCTIONBACK) => {
             history.pushState({ data: FUNCTION() }, "", "file:///android_res/");
         } else {
             history.replaceState({ data: FUNCTION() }, "", "file:///android_res/");
-        }
+        };
     } else {
         if (NEWPAGE) {
             history.pushState({ data: FUNCTION() }, "", "");
         } else {
             history.replaceState({ data: FUNCTION() }, "", "");
-        }
-    }
+        };
+    };
     window.addEventListener("popstate", function (event) {
         const previousPageFunction = sessionStorage.getItem("PreviousPage");
         if (previousPageFunction) {
             const func = new Function("return " + previousPageFunction)();
             func();
-        }
+        };
     });
 };
 export{NAVIGATOR};
--- Project/Project.js
+++ Project/Project.js
@@ -17,6 +17,6 @@ const AUTORUN=()=>{
         {
             "Name":"Hame"
         },
-    ]
+    ];
 
 };
