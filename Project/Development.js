f61599e7e1345b1436937292e37e0fa2c80b7acc
EroIndustries <127468229+EroIndustries@users.noreply.github.com>
2025-01-10 07:23:53 +0300

    Pages
    We have Introduced Pages

--- Project/Development.js
+++ Project/Development.js
@@ -2,24 +2,78 @@ const ANDROIDLOGINPAGE=()=>{
 
     CLEAR('');
 
-    ROUNDINPUT('','email','UserEmail','',(data)=>{
+    ICON('',WHITEAPPICON,(ELEMENT)=>{
+
+        STYLED(ELEMENT,'width','30%');
+        STYLED(ELEMENT,'height','20%');
+        STYLED(ELEMENT,'margin-top','10%');
+        STYLED(ELEMENT,'margin-bottom','10%');
+        STYLED(ELEMENT,'margin-left','30%');
 
     });
 
-    ROUNDINPUT('','email','UserEmail','',(data)=>{
+    ROUNDINPUT('','email','Enter Your Email','',(data)=>{
 
     });
 
-    BUTTON('','Sign In','','forestgreen',()=>{
+    ROUNDINPUT('','password','*********','',(data)=>{
+
+    });
+
+    BUTTON('','Sign In','','forestgreen',(ELEMENT)=>{
 
     },()=>{
         
     });
 
-    BUTTON('','Sign In','','blue',()=>{
+    BUTTON('','Create Account','','blue',()=>{
+
+        NAVIGATOR('Yes',ANDROIDCREATEPAGE,'ANDROIDLOGINPAGE');
 
     },()=>{
         
     });
 
-}
+};
+
+const ANDROIDCREATEPAGE=()=>{
+
+    CLEAR('');
+
+    ICON('',WHITEAPPICON,(ELEMENT)=>{
+
+        STYLED(ELEMENT,'width','30%');
+        STYLED(ELEMENT,'height','20%');
+        STYLED(ELEMENT,'margin-top','10%');
+        STYLED(ELEMENT,'margin-bottom','10%');
+        STYLED(ELEMENT,'margin-left','30%');
+
+    });
+
+    ROUNDINPUT('','','Enter Your Name','',(data)=>{
+
+    });
+
+    ROUNDINPUT('','email','Enter Your Email','',(data)=>{
+
+    });
+
+    ROUNDINPUT('','password','*********','',(data)=>{
+
+    });
+
+    BUTTON('','Sign Up','','forestgreen',()=>{
+
+    },()=>{
+        
+    });
+
+    BUTTON('','Log In','','blue',()=>{
+
+        NAVIGATOR('',ANDROIDLOGINPAGE,'ANDROIDLOGINPAGE');
+
+    },()=>{
+        
+    });
+
+};
